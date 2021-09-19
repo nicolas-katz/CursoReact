ff75c2450a35e51f2ea81f151677cd2922cbe88f
nicolas-katz <nicokatz12@gmail.com>
2021-09-19 20:38:18 -0300

    desafio 5

--- src/components/ListProducts/ListProducts.js
+++ src/components/ListProducts/ListProducts.js
@@ -5,12 +5,12 @@ import CardComponent from '../Product/Product';
 const ListProducts = ()=> {
     return (
         <div className="CardsContainer">
-            <CardComponent title="producto 1" text="Descripción producto 1" button="Agregar al carrito" />
-            <CardComponent title="producto 2" text="Descripción producto 2" button="Agregar al carrito" />
-            <CardComponent title="producto 3" text="Descripción producto 3" button="Agregar al carrito" />
-            <CardComponent title="producto 4" text="Descripción producto 4" button="Agregar al carrito" />
-            <CardComponent title="producto 5" text="Descripción producto 5" button="Agregar al carrito" />
-            <CardComponent title="producto 6" text="Descripción producto 6" button="Agregar al carrito" />
+            <CardComponent title="producto 1" text="Descripción producto 1" />
+            <CardComponent title="producto 2" text="Descripción producto 2" />
+            <CardComponent title="producto 3" text="Descripción producto 3" />
+            <CardComponent title="producto 4" text="Descripción producto 4" />
+            <CardComponent title="producto 5" text="Descripción producto 5" />
+            <CardComponent title="producto 6" text="Descripción producto 6" />
         </div> 
     )
 }
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,12 +1,17 @@
 import './Product.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 const CardComponent = prop => {
+    const [productos, setProductos] = useState(0);
+    const productosSeleccionados = ()=>{
+      setProductos(productos + 1);
+    }
     return(
       <div className="Card">
         <h3>{prop.title}</h3>
         <p>{prop.text}</p>
-        <button>{prop.button}</button>
+        <button onClick={productosSeleccionados}>Agregar al carrito</button>
+        <p>Cantidad de productos seleccionados: {productos}</p>
       </div>
     )
 }
