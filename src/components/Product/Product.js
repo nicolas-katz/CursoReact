0229d1434a1f69d34fb3d3783e3525e8a7e88771
nicolas-katz <nicokatz12@gmail.com>
2021-09-20 23:43:56 -0300

    desafio 5 con cambios

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,15 +3,28 @@ import React, { useState } from 'react';
 
 const CardComponent = prop => {
     const [productos, setProductos] = useState(0);
-    const productosSeleccionados = ()=>{
+    const sumarProductos = ()=>{
       setProductos(productos + 1);
+      if (productos >= 10){
+        setProductos(10)
+      }
+    }
+    const restarProductos = ()=>{
+      setProductos(productos - 1);
+      if (productos <= 0){
+        setProductos(0)
+      }
     }
     return(
       <div className="Card">
         <h3>{prop.title}</h3>
         <p>{prop.text}</p>
-        <button onClick={productosSeleccionados}>Agregar al carrito</button>
-        <p>Cantidad de productos seleccionados: {productos}</p>
+        <button>Agregar al carrito</button>
+        <div className="AgregarProductos">
+          <button onClick={restarProductos}>-</button>
+          <p>{productos}</p>
+          <button onClick={sumarProductos}>+</button>
+        </div>
       </div>
     )
 }
