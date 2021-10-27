7cfdc3bcc083052384eb78044dffe8a6e9efca42
nicolas-katz <nicokatz12@gmail.com>
2021-10-27 17:32:53 -0300

    avances proyecto

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_FIREBASE_APPID
+  apiKey: "AIzaSyB9jMZ_kDosejCOWdDwjJRst7YENVysibY",
+  authDomain: "coderhouse-project-3d62b.firebaseapp.com",
+  projectId: "coderhouse-project-3d62b",
+  storageBucket: "coderhouse-project-3d62b.appspot.com",
+  messagingSenderId: "114853500295",
+  appId: "1:114853500295:web:0e5d0e47ba8b76431a6f22",
+  measurementId: "G-07WQ70G749"
 };
 
 const app = initializeApp(firebaseConfig);
